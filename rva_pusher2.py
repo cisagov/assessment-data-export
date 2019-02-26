fc672f32c2a29c53852a223a72548c29851c5536
Scott Wallace <scottwallace@rocketmail.com>
2019-02-26 15:20:29 -0500

    Major Update

--- rva_pusher2.py
+++ rva_pusher2.py
@@ -20,127 +20,98 @@ from cyhy.db import database
 from cyhy.util import util
 import subprocess
 from datetime import datetime
+import dateutil.parser
+
 
 FMT = '%m/%d/%y %H:%M'
+JIRA_FILE = 'exported-rva-data.csv'
 
 # Pull the data fresh from JIRA
 def grab_csv_from_jira(filter):
-    f = open('account.txt','r')
+    f = open('../account.txt','r')
     lines = f.readlines()
-    username = lines[0]
-    password = lines[1]
+    username = lines[0].rstrip()
+    password = lines[1].rstrip()
     f.close()
-    # TODO 12603 - make sure last column shows (will need to be consolidated)
-    # TODO add username and password
-    subprocess.call(['curl -k {0}:{0} https://jira.ncats.cyber.dhs.gov/sr/jira.issueviews:searchrequest-csv-current-fields/{0}/SearchRequest-{0}.csv\?delimiter\=, -o exportedReport.csv'.format(username,password,filter,filter)], shell=True)
+    
+    subprocess.call(['curl -k {}:{} https://jira.ncats.cyber.dhs.gov/sr/jira.issueviews:searchrequest-csv-current-fields/{}/SearchRequest-{}.csv\?delimiter\=, -o {}'.format(username,password,filter,filter,JIRA_FILE)], shell=True)
 
 # Strip out custom fields & consolidate column names
-def csv_refining():
-    inputFileName = "exportedReport.csv"
-    outputFileName = os.path.splitext(inputFileName)[0] + "_modified.csv"
-
-    with open(inputFileName, 'rb') as inFile, open(outputFileName, 'wb') as outfile:
-        r = csv.reader(inFile)
-        w = csv.writer(outfile)
-
-        next(r, None)  # skip the first row from the reader, the old header
-        # write new header
-        w.writerow(['Summary','Status','Created','Updated','Appendix A Date','Appendix A Signed','Appendix B Signed','Assessment Type','External Testing Begin Date','External Testing End Date','Group/Project','Internal Testing Begin Date','Internal Testing City','Internal Testing End Date','Mgmt Req','POC Email','POC Name','POC Phone','ROE Date','ROE Number','ROE Signed','_id','Asmt Name','Requested Services1','Requested Services2','Requested Services3','Requested Services4','Requested Services5','Requested Services6','Requested Services7','Requested Services8','Stakeholder Name','State','Testing Complete Date','Testing Phase','Election','Testing Sector', 'CI Type', 'CI Systems', 'Fed Lead','Contractor Operator Count','Draft w/ POC Date','Fed Operator Count','Report Final Date','Operator1', 'Operator2', 'Operator3', 'Operator4', 'Operator5','Stakeholder ID','Testing Begin Date'])
-
-        # copy the rest
-        for row in r:
-            # Date row for strings into datetimes
-            for cell in (3, 4, 5, 9, 10, 11, 12, 14, 18, 34):
-                try:
-                    row[cell-1] = datetime.strptime(row[cell-1], FMT).isoformat()
-                except:
-                    pass
-            # Boolean row for strings into bools     
-            for cell in (6, 7, 21):
-                if row[cell-1] != '':
-                    if row[cell-1] == 'True':
-                        row[cell-1] = True
-                    elif row[cell-1] == 'False':
-                        row[cell-1] = False
-            
-            w.writerow(row)
-            
-        print '\n\nSuccessfully modified\n\n'
+def prep_and_import_csv(db):
+    assessments = open(JIRA_FILE)
+    # Create a writer so we have a list of our unique agencies
+    domains_processed = 0
     
-def import_csv_to_mongo(db):
-    # mongoimport -d mydb -c rvaInput --type csv --file exportedReport.csv --headerline
-    
-    #import IPython; IPython.embed() #<<< BREAKPOINT >>>
-    subprocess.call('mongoimport -d mydb -c rvaInput --type csv --file exportedReport_modified.csv --headerline'.format(db.name), shell=True)
-    return 0
-    
-def change_ids_and_arrays(db):
-    collection = db.rva
-    
-    for i in collection.find():
-        print i        
-        doc = i
-        old_id = i['_id']
-    
-        requested_services = [
-            i['Requested Services1'],
-            i['Requested Services2'],
-            i['Requested Services3'],
-            i['Requested Services4'],
-            i['Requested Services5'],
-            i['Requested Services6'],
-            i['Requested Services7'],
-            i['Requested Services8']
-        ]
-    
-        operators = [
-            i['Operator1'],
-            i['Operator2'],
-            i['Operator3'],
-            i['Operator4'],
-            i['Operator5']
-        ]
-    
-        # insert the document, using the new _id
-        del doc['Requested Services1']
-        del doc['Requested Services2']
-        del doc['Requested Services3']
-        del doc['Requested Services4']
-        del doc['Requested Services5']
-        del doc['Requested Services6']
-        del doc['Requested Services7']
-        del doc['Requested Services8']
-        del doc['Operator1']
-        del doc['Operator2']
-        del doc['Operator3']
-        del doc['Operator4']
-        del doc['Operator5']
+    firstline = True
+    for row in csv.reader(assessments):
+        if firstline:    #skip first line
+            firstline = False
+            continue
+        # parse the line
+        for j in (2,3,4,8,9,11,13,18,33,41,43,50):
+            row[j] = dateutil.parser.parse(row[j])
+        for k in (5,6,20):
+            if row[k].upper() == 'TRUE':
+                row[k] = True
+            elif row[k].upper() == 'FALSE':
+                row[k] = False
+            else:
+                row[k] = None
+        for o in (19,40,42):
+            try:
+                row[o] = int(float(row[o]))
+            except:
+                continue
         
-        collection.remove({'_id': old_id})
-        collection.remove({'_id': doc['_id']})
-        
-        try:
-            collection.insert(doc)
+        # 'Summary','Status','Created','Updated','Appendix A Date','Appendix A Signed','Appendix B Signed','Assessment Type','External Testing Begin Date','External Testing End Date','Group/Project','Internal Testing Begin Date','Internal Testing City','Internal Testing End Date','Mgmt Req','POC Email','POC Name','POC Phone','ROE Date','ROE Number','ROE Signed','_id','Asmt Name','Requested Services1','Requested Services2','Requested Services3','Requested Services4','Requested Services5','Requested Services6','Requested Services7','Requested Services8','Stakeholder Name','State','Testing Complete Date','Testing Phase','Election','Testing Sector', 'CI Type', 'CI Systems', 'Fed Lead','Contractor Operator Count','Draft w/ POC Date','Fed Operator Count','Report Final Date','Operator1', 'Operator2', 'Operator3', 'Operator4', 'Operator5','Stakeholder ID','Testing Begin Date'
+        try:    
+            db.rva.insert_one({
+                '_id': row [21], # switched with 21
+                "status": row[1],
+                "created": row[2],
+                "updated": row[3],
+                "appendix_a_date": row[4],
+                "appendix_a_signed": row[5],
+                "appendix_b_signed": row[6],
+                "assessment_type": row[7],
+                "external_testing_begin_date": row[8],
+                "external_testing_end_date": row[9],
+                "group": row[10],
+                "internal_testing_begin_date": row[11],
+                "internal_testing_city": row[12],
+                "internal_testing_end_date": row[13],
+                "mgmt_req": row[14],
+                'poc_email': row[15],
+                'poc_name': row[16],
+                'poc_phone': row[17],
+                'roe_date': row[18],
+                'roe_number': row[19],
+                'roe_signed': row[20],
+                "summary": row[0],
+                'asmt_name': row[22],
+                'requested_services': [row[23],row[24],row[25],row[26],row[27],row[28],row[29],row[30]],
+                'stakeholder_name': row[31],
+                'state': row[32],
+                'testing_complete_date': row[33],
+                'testing_phase': row[34],
+                'election': row[35],
+                'testing_sector': row[36], 
+                'ci_type': row[37], 
+                'ci_systems': row[38], 
+                'fed_lead': row[39],
+                'contractor_operator_count': row[40],
+                'draft_poc_date': row[41],
+                'fed_operator_count': row[42],
+                'report_final_date': row[43],
+                'operators': [row[44],row[45],row[46],row[47],row[48]],
+                'stakeholder_id': row[49],
+                'testing_begin_date': row[50]
+            })
+            domains_processed += 1
         except:
-            collection.remove({'_id': doc['_id']})
-            
-        # mycollection.update({'_id':mongo_id}, {"$set": post}, upsert=False)
-        for j in ['Mgmt Req','Appendix A Signed','ROE Signed']:
-            if i[j].upper() == 'FALSE':
-                collection.update({'_id':doc['_id']}, {'$set': {j:0}}, upsert=False)
-                #i.update({'$set':{j:0}})
-            elif i[j].upper() == 'TRUE':
-                collection.update({'_id':doc['_id']}, {'$set': {j:0}}, upsert=False)
-                #i.update({'$set':{j:1}})
-        
-        for k in ['External Testing End Date','External Testing Begin Date','Created','Internal Testing End Date','Internal Testing Begin Date','Testing Begin Date','Testing Complete Date','ROE Date','Updated']:
-            collection.update({'_id': doc['_id'] },{'$set' : {k:datetime.strptime(doc[k], "%Y-%m-%d")}})
-    
-        collection.update({'_id': doc['_id'] },{'$set' : {'Requested Services':requested_services}})
-        collection.update({'_id': doc['_id'] },{'$set' : {'Operators':operators}})
-        
-def sector_FY_stats():
-    return 0
+            print row[0] + ' is already a key in the DB'
+
+    print('Successfully imported {} documents to "{}" database on {}'.format(domains_processed, db.name, db.client.address[0]))
 
 def main():
     global __doc__    
@@ -149,9 +120,7 @@ def main():
     db = database.db_from_config(args['--section'])
     
     grab_csv_from_jira(args['FILTER'])
-    csv_refining()
-    import_csv_to_mongo(db)
-    change_ids_and_arrays(db)
+    prep_and_import_csv(db)
     
 if __name__=='__main__':
     main()
