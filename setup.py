cbe0f1a53ad7cc924b44bf52c36129704d4e4d9a
Dave Redmin <david.redmin@beta.dhs.gov>
2019-05-07 23:38:27 -0400

    Create setup.py and adjust requirements files to match our team standards

---
+++ setup.py
@@ -0,0 +1,73 @@
+"""
+This is the setup module for the example project.
+
+Based on:
+
+- https://packaging.python.org/distributing/
+- https://github.com/pypa/sampleproject/blob/master/setup.py
+- https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure
+"""
+
+from glob import glob
+from os.path import splitext, basename
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    """Read in and return the contents of the project's README.md file."""
+    with open("README.md") as f:
+        return f.read()
+
+
+def package_vars(version_file):
+    """Read in and return the variables defined by the version_file."""
+    pkg_vars = {}
+    with open(version_file) as f:
+        exec(f.read(), pkg_vars)  # nosec
+    return pkg_vars
+
+
+setup(
+    name="ade",
+    # Versions should comply with PEP440
+    version=package_vars("src/ade/_version.py")["__version__"],
+    description="Exports assessment data from Jira to an AWS S3 bucket",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    # NCATS "homepage"
+    url="https://www.us-cert.gov/resources/ncats",
+    # The project's main homepage
+    download_url="https://github.com/cisagov/assessment-data-export",
+    # Author details
+    author="Cyber and Infrastructure Security Agency",
+    author_email="ncats@hq.dhs.gov",
+    license="License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 4 - Beta",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        # Pick your license as you wish (should match "license" above)
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    # What does your project relate to?
+    keywords=["ade", "assessment", "export"],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    include_package_data=True,
+    install_requires=["boto3", "docopt", "python-dateutil", "requests", "xmljson"],
+    extras_require={"test": ["pre-commit", "pytest", "pytest-cov", "coveralls"]},
+    # Conveniently allows one to run the CLI tool as `ade`
+    entry_points={"console_scripts": ["ade = ade.assessment_data_export:main"]},
+)
